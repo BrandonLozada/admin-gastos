c922f0ab5608482098ffa8efd207b24ef1a87b7d
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-12 15:25:50 -0600

    Subir avance a remoto

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script setup>
+import { ref } from 'vue'
+import Presupuesto from './components/Presupuesto.vue'
+import ControlPresupuesto from './components/ControlPresupuesto.vue';
+
+const presupuesto = ref(0)
+const disponible = ref(0)
+
+const definirPresupuesto = (cantidad) => {
+  presupuesto.value = cantidad
+  disponible.value = cantidad
+}
 </script>
 
 <template>
   <div>
     <header>
       <h1>Planificador de gastos</h1>
+      <div class="contenedor-header contenedor sombra">
+        <Presupuesto
+          v-if="presupuesto === 0"
+          @definir-presupuesto="definirPresupuesto"
+        />
+
+        <ControlPresupuesto
+          v-else
+          :presupuesto="presupuesto"
+          :disponible="disponible"
+        />
+
+      </div>
     </header>
 
   </div>
@@ -14,7 +38,7 @@
   :root {
     --azul: #3b82f6;
     --blanco: #FFF;
-    --gris-claro: #f5f5f5;
+    --gris-claro: #F5F5F5;
     --gris: #94a3b8;
     --gris-oscuro: #64748b;
     --negro: #000;
@@ -25,7 +49,7 @@
   }
   *,
   *:before,
-  *:before {
+  *:after {
     box-sizing: inherit;
   }
   body {
@@ -48,5 +72,20 @@
     color: var(--blanco);
     text-align: center;
   }
-
+  .contenedor {
+      width: 90%;
+      max-width: 80rem;
+      margin: 0 auto;
+  }
+  .contenedor-header {
+    margin-top: -5rem;
+    transform: translateY(5rem);
+    padding: 5rem;
+  }
+  .sombra {
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+    background-color: var(--blanco);
+    border-radius: 1.2rem;
+    padding: 5rem;
+  }
 </style>
