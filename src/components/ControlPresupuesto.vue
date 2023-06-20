932baedd4a09e72894dc49ad20292856d2d42ad4
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-19 20:38:00 -0600

    Graficar componente del paquete de vue3-circle-progress

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,7 @@
 <script setup>
-import Imagen from '../assets/img/grafico.jpg'
+import { computed } from 'vue'
+import CircleProgress from 'vue3-circle-progress'
+import "vue3-circle-progress/dist/circle-progress.css"
 import { formatearCantidad } from "../helpers";
 
 defineEmits(['reset-app'])
@@ -17,15 +19,26 @@ const props = defineProps({
         required: true
     }
 })
+
+const porcentaje = computed(() => {
+  return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
+})
 </script>
 
 <template>
     <div class="dos-columnas">
         <div class="contenedor-grafico">
-            <img
-                :src="Imagen"
-                alt="imagen gráfico"
-            />
+
+          <p class="porcentaje">{{ porcentaje }}%</p>
+
+          <CircleProgress
+            :percent="porcentaje"
+            :size="250"
+            :border-width="30"
+            :border-bg-width="30"
+            fill-color="#3b82f6"
+            empty-color="#E1E1E1"
+          />
         </div>
 
         <div class="contenedor-presupuesto">
@@ -57,6 +70,21 @@ const props = defineProps({
 
 
 <style scoped>
+    .contenedor-grafico {
+      position: relative;
+    }
+    .porcentaje {
+      position: absolute;
+      margin: auto;
+      top: calc(50% - 1.5rem);
+      left: 0;
+      right: 0;
+      text-align: center;
+      z-index: 100;
+      font-size: 3rem;
+      font-weight: 900;
+      color: var(--gris-oscuro);
+    }
     .dos-columnas {
         display: flex;
         flex-direction: column;
